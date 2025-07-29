457c62bc044e9294a5b763fbac124bfad58476bd
iandreagrant <ivannapani+github@gmail.com>
2025-07-29 16:08:02 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -5,8 +5,17 @@ let add3El = document.getElementById("home-add3-btn")
 let homeScoreEl = document.getElementById("home-score")
 let guestScoreEl = document.getElementById("guest-score")
 
-let homeContainer = document.getElementById("home-container");
-let guestContainer = document.getElementById("guest-container");
+let homeContainer = document.getElementById("home-container")
+let guestContainer = document.getElementById("guest-container")
+
+let winnerEl = document.getElementById("winner-container")
+let containerEl = document.getElementById("container")
+
+let winnerText = document.getElementById("winner-text")
+let winnerScore = document.getElementById("score-display")
+
+let confettiCanvas = document.getElementById('confetti-canvas')
+let myConfetti = confetti.create(confettiCanvas, { resize: true, useWorker: true })
 
 let homeCurrentScore = 0
 let guestCurrentScore = 0
@@ -54,6 +63,9 @@ function newGame() {
     guestScoreEl.textContent = guestCurrentScore  
     homeScoreEl.classList.remove("highlight")
     guestScoreEl.classList.remove("highlight")
+    
+    containerEl.classList.remove("hidden")
+    winnerEl.classList.add("hidden")
 }
 
 function updateScores() {
@@ -77,4 +89,43 @@ function updateLeaderHighlight() {
     homeScoreEl.classList.remove("highlight")
     guestScoreEl.classList.remove("highlight")
   }
+}
+
+function endGame() {
+    containerEl.classList.add("hidden");
+    winnerEl.classList.remove("hidden");
+    
+    let winner = ''
+    
+    if (homeCurrentScore > guestCurrentScore) {
+        winner = 'Home'
+    } 
+    
+    else if (guestCurrentScore > homeCurrentScore) {
+        winner = 'Guest'
+    } 
+    
+    else {
+        winner = 'No one. It\'s a tie'
+    }
+    
+    winnerText.textContent = 
+        winner === 'No one. It\'s a tie'
+                ? "It's a tie!"
+                : `You're a winner, ${winner}! 🎉`
+                
+    winnerScore.textContent =
+        `Final Score — Home: ${homeCurrentScore} | Guest: ${guestCurrentScore}`
+        
+    if (winner !== 'No one. It\'s a tie') {
+        launchConfetti();
+    }
+}
+
+function launchConfetti() {
+    myConfetti({
+        particleCount: 150,
+        spread: 70,
+        origin: { y: 0.6 }
+    })
 }
